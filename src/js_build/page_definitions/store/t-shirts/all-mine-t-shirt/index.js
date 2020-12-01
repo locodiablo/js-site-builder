e86490f84d520612e521129288730f1375c88fc3
Simon Rollett <madjester@Simons-MacBook-Air.local>
2020-12-01 18:26:20 +0000

    Updated style for main nav, added new product page

--- src/js_build/page_definitions/store/t-shirts/all-mine-t-shirt/index.js
+++ src/js_build/page_definitions/store/t-shirts/all-mine-t-shirt/index.js
@@ -1,15 +1,15 @@
 const content = {
   config: {
     html_template: "template_page_banner",
-    nav_text: "T-shirts",
+    nav_text: "All Mine",
     body_class: ""
   },
   head: {
-    title: "T-shirts",
-    description: "MadJester t-shirts are designed and built with purpose in mind: comfort fit, durable fabric, sentry guns in each armpit. (May not have sentry guns in arm pits)"
+    title: "All Mine t-shirt",
+    description: "All about the Original t-shirt custom products description Ut enim ad minim veniam"
   },
   body: [
-    "Custom products listed here would be nice",
+    "All Mine desc",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   ],
   footer: []
--- src/js_build/partials/footer.js
+++ src/js_build/partials/footer.js
@@ -5,7 +5,7 @@ const vars = require("../vars.js")
 function footer(data){return `
   <footer class="footer py-4" role="contentinfo">
       <div class="container">
-          &copy;${vars.date.year} ${defaults.site_name_full} All rights reserved.
+          &copy;${vars.date.year} ${defaults.site_name_full}. All rights reserved.
       </div>
   </footer>
   ${modal(data)}
