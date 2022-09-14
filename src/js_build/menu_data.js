60ce9f51c981342f4afb7d9d7afaca767f7b1eb0
Loco Diablo <simon@madjester.co.uk>
2022-09-15 00:18:13 +0100

    updated menu array loop

--- src/js_build/menu_data.js
+++ src/js_build/menu_data.js
@@ -2,7 +2,7 @@ const dirTree = require("directory-tree");
 const paths = require("../js_build/paths.js");
 const menuData = dirTree(`./${paths.definitionsDir}`, { extensions:/\.js/,exclude: /404.js/,attributes: ['type'] }).children;
 
-console.log('menuData',menuData)
+console.log('menuData',menuData.length,menuData)
 
 const menu_data = {
 
--- src/js_build/partials/nav_main.js
+++ src/js_build/partials/nav_main.js
@@ -4,40 +4,12 @@ const nav_breadcrumb = require("../partials/nav_breadcrumb.js");
 const dirTree = require("directory-tree");
 const menuData = require("../menu_data.js");
 
-// const navOutputSplitORIGINAL = {
-//   directory: function(incomingLinkData){
-//     const navUrl = incomingLinkData.data.path.replace(paths.definitionsDir,"")
-//     incomingLinkData.data.children.length > 1 ? incomingLinkData.exploreClass = vars.css_class_nav_with_children : ''
-//     return `
-//     <li class="nav-item nav-dir got-child nav-item-${incomingLinkData.thisLinkCount}">
-//       <a class="nav-link ${incomingLinkData.exploreClass}" data-my-menu="${incomingLinkData.thisLinkCount}" href="${navUrl}">
-//         ${incomingLinkData.data.pageData.text}
-//       </a>
-//       <ul>
-//       ${navOutputSplit[incomingLinkData.data.children]}
-//       </ul>
-//     </li>
-//     `
-//     incomingLinkData.children.map(dataChild =>{
-//       navOutputSplit[dataChild.type](dataChild)
-//     }).join("")
-//
-//   },
-//   file: function(incomingLinkData){
-//     //console.log(`file: ${incomingLinkData.data.path}`)
-//     return `
-//     <li class="no-child">
-//       <a href="">${incomingLinkData.data.name}</a>
-//     </li>`
-//   }
-// }
-
 const templates = {
   directory: function(data){
     return `
     <ul class="nav-item-children">
       ${data.map(nav_item => {
-        console.log(`${nav_item.name} with children: ${nav_item.name}`);
+        console.log(`${nav_item.name} dir with children: ${nav_item.name}`);
         return renderNavItem(nav_item)
       }).join("")}
     </ul>
@@ -57,17 +29,25 @@ function renderNavItem(data){
 }
 
 function render_nav_items(data){
+  // for (const nav_item in data) {
+  //   console.log('render nav items:',data[nav_item].name)
+  //   return renderNavItem(data[nav_item])
+  // }
+  console.log('item')
+
+
+  return data.map(nav_item => {
+    console.log(`${nav_item.name} with children: ${nav_item.name}`);
+    return renderNavItem(nav_item)
+  }).join("")
+}
+
+function render_nav_itemsWORKING(data){
   for (const nav_item in data) {
     console.log('render nav items:',data[nav_item].name)
     return renderNavItem(data[nav_item])
   }
 }
-
-
-
-
-
-//menuData.map((eachItem,index) => navTreeSplit[eachItem.type](eachItem)).join("")
 
 function navbar(data,incomingMenuData){return `
   <nav class="navbar navbar-expand-sm bg-light">
